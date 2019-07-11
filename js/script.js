9a0ed975abaf11582c52963cc9ad5cf2de13ba49
Kiotlin <719401217@qq.com>
2019-07-12 03:12:16 +0800

    Add js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
+
 $(document).ready(function(){
+
+    $(function(){
+        $(window).scroll(function() {
+            if ($(document).scrollTop() > 50) {
+                $('header').addClass('sticky');
+                $('.logo_wrap a h1 i').hide();
+            } else {
+                $('header').removeClass('sticky');
+                $('.logo_wrap a h1 i').show();
+            }
+        });
+    });
+
     $("#go_top").hide();
     $(function(){
         $(window).scroll(function(){
-            if($(this).scrollTop() > 50){
+            if($(this).scrollTop() > 250){
                 $("#go_top").fadeIn();
             }else {
                 $("#go_top").fadeOut();
